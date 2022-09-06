5251c005860d43d7c94c64e10bf0987895357c9d
Thiago Nunes da Cruz <84878544+Thiagonns@users.noreply.github.com>
2022-09-06 17:06:43 -0400

    Create Script - CREATE TABLE Subcategoria.sql
    Criando tabela Subcategoria, com FK apontando para Categoria.

---
+++ SQL/Encontro_02/Script - CREATE TABLE Subcategoria.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Subcategoria(
+    Codigo INT NOT NULL IDENTITY(1,1),
+    CodigoCategoria INT NOT NULL,
+    Descricao VARCHAR (MAX) NOT NULL,
+    CONSTRAINT PK_Subcategoria PRIMARY KEY (Codigo),
+    CONSTRAINT FK_Subcategoria_Categoria FOREIGN KEY (CodigoCategoria) REFERENCES Categoria(Codigo)
+)
+GO
