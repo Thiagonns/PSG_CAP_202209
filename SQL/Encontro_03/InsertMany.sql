c45ad4395987a1c8de16428e04c09faca80d7b2f
Thiago Nunes da Cruz <84878544+Thiagonns@users.noreply.github.com>
2022-09-08 17:24:14 -0400

    Create InsertMany.sql

---
+++ SQL/Encontro_03/InsertMany.sql
@@ -0,0 +1,11 @@
+INSERT INTO Cliente(Nome,Documento,Telefone,Email,Endereco)
+VALUES
+('Mateus','123','123','mateus@teste.com','Rua x, n. 10'),
+('Lucas','456','987654321','lucas@teste.com','Rua x, n. 11'),
+('joao','789','98767544d1','joao@teste.com','Rua x, n. 12'),
+('Pedro','487','987647321','pedro@teste.com','Rua x, n. 13'),
+('Tiago','473','985587496','tiago@teste.com','Rua x, n. 14'),
+('Judas','668','987654321','judas@teste.com','Rua x, n. 15'),
+('Marcos','716','943824321','lucas@teste.com','Rua x, n. 16'),
+('Andre','457','9774576221','andre@teste.com','Rua x, n. 17')
+GO
