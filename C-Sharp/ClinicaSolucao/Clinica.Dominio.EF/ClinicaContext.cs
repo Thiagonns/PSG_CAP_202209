89aae453a0b8d3d9c8b390eb0ae0b316abb2f49e
Thiago Nunes da Cruz <thiagonunesc97@gmail.com>
2022-11-23 10:03:17 -0400

    Encontro_50
    Realizada a implementação do projeto Clinica. Como também as camadas EF, Poco, Repository e Mapping.

---
+++ C-Sharp/ClinicaSolucao/Clinica.Dominio.EF/ClinicaContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.Dominio.EF
+{
+    public partial class ClinicaContext : DbContext
+    {
+        public DbSet<Paciente> pacientes { get; set; } = null!;
+        protected ClinicaContext() : base()
+        {
+        }
+        public ClinicaContext(DbContextOptions<ClinicaContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // optionsBuilder.UseSqlServer("Data Source=psgs0071.psg.local; Initial Catalog=Academia;User=academia;Password=@cadem1@555;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Paciente>(entity =>
+            {
+                entity.Property(e => e.DataInclusao).HasDefaultValueSql("(getdate())");
+                entity.Property(e => e.Situacao).HasDefaultValueSql("((1))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
